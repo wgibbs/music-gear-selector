63c64139798832fcba5037036fff24b6a8269e32
Wesley Gibbs <wcgibbsey@gmail.com>
2016-04-15 17:47:21 -0400

    added unique filter and css updates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,8 @@ var musicgearApp = angular.module('musicgearApp', []);
 
 musicgearApp.controller('MusicItemCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('items.json').success(function(data) {
-    $scope.filters = {};
     $scope.items = data;
+    $scope.filters = {};
   });
 }]);
 
---
+++ app/js/unique.js
@@ -0,0 +1,17 @@
+//Unique Filter which returns only one of each category for nav items
+musicgearApp.filter('unique', function() {
+   return function(collection, keyname) {
+      var output = [], 
+          keys = [];
+
+      angular.forEach(collection, function(item) {
+          var key = item[keyname];
+          if(keys.indexOf(key) === -1) {
+              keys.push(key);
+              output.push(item);
+          }
+      });
+
+      return output;
+   };
+});
