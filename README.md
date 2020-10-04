c45e27c52967e879cd645672b5e7cbb7cb281ab1
Wesley Gibbs <wcgibbsey@gmail.com>
2020-10-04 08:23:40 -0400

    Gulp Updates

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # music-gear-selector
-A single page responsive music gear selector web app built with AngularJS. This project can be viewed here: http://wgibbs.com/music-gear-selector/app/
+A single page responsive music gear selector web app built with AngularJS. This project can be viewed here: https://wgibbs.com/music-gear-selector/app/
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,44 @@
-'use strict';
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  path = require('path'),
+  moduleImporter  = require('sass-module-importer'),
+  webserver = require('gulp-webserver'),
+  autoprefixer = require('gulp-autoprefixer'),
+  buffer = require('vinyl-buffer'),
+  source = require('vinyl-source-stream');
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var webserver = require('gulp-webserver');
+function swallowError (error) {
+  console.log(error.toString());
+  this.emit('end');
+}
 
-var paths = {
-  styles: ['./app/sass/app.sass'],
+const config = {
+  paths: {
+    scssSource: './sass/',
+    cssDest: './css',
+  },
   root: ['./app']
 };
 
-gulp.task('sass', function(){
-  return gulp.src(paths.styles)
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./app/css/'))
+gulp.task('styles', () => {
+  return gulp.src(path.join(config.paths.scssSource, 'main.scss'))
+  .pipe(sass({importer: moduleImporter(), outputStyle: 'compressed'})
+  .on('error', swallowError))
+  .pipe(gulp.dest(config.paths.cssDest));
 });
 
-gulp.task('webserver', function(){
-  gulp.src(paths.root)
-    .pipe(webserver({
-      fallback: 'index.html',
-      livereload: true
-    }));
+gulp.task('serve', () => {
+  gulp.src(config.root)
+  .pipe(webserver({
+    fallback: './app/index.html',
+    livereload: true,
+    open: true
+  }))
+  .pipe(autoprefixer({
+    browsers: ['> 1%', 'last 5 versions', 'Firefox >= 20', 'iOS >=7'],
+    cascade: false
+  }))
+  gulp.watch(path.join(config.paths.scssSource, '**/*.scss'), ['styles']);
 });
 
-gulp.task('default', ['sass', 'webserver']);
+gulp.task('default', ['styles', 'serve']);
